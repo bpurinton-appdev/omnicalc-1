0ad9789a43367d81f200e99bec4fe0dd09d92b10
Ben Purinton <ben.purinton@gmail.com>
2022-11-28 22:31:58 +0000

    square is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ :template => "calc_templates/square_form.html.erb" })
+  end
 
+  def calc_square
+    @new = params.fetch("user_input")
+    @square = @new.to_f ** 2
+    render({ :template => "calc_templates/square_result.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+  get("/square/result", { :controller => "application", :action => "calc_square" })
 end
